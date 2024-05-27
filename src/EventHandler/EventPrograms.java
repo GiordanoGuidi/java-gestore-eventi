b31a16560a146f33ad395a898ef5dda145a74d55
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-27 17:26:15 +0200

    added class EventPrograms

---
+++ src/EventHandler/EventPrograms.java
@@ -0,0 +1,17 @@
+package EventHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventPrograms {
+    //Attributi
+    String title;
+    List<Event> events;
+    //Costruttori
+    public EventPrograms(String title, List<Event> events) {
+        this.title = title;
+        this.events = new ArrayList<>();
+    }
+
+    //Metodi
+}
